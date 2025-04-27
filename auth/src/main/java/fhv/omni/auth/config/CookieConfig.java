727d6ab257588098536ca3e0e9bf132997a86050
Saveli Gulas <saveli.gulas@gmail.com>
2025-04-27 23:26:22 +0200

    added a CookieSerializer configuration

---
+++ auth/src/main/java/fhv/omni/auth/config/CookieConfig.java
@@ -0,0 +1,21 @@
+package fhv.omni.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+public class CookieConfig {
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("JSESSIONID");
+        serializer.setCookiePath("/");
+        serializer.setSameSite("Lax");
+        serializer.setUseHttpOnlyCookie(true);
+        serializer.setUseSecureCookie(false); // Set to true for HTTPS
+        return serializer;
+    }
+}
