42c7c564b9f55b90946946583ab87e9ed506b6fd
Saveli Gulas <saveli.gulas@gmail.com>
2025-04-27 15:22:07 +0200

    added redis, postgresql, lombok, session-redis to auth build.gradle

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -21,8 +21,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.projectlombok:lombok")
+	implementation("org.springframework.session:spring-session-data-redis")
+	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	runtimeOnly("org.postgresql:postgresql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
