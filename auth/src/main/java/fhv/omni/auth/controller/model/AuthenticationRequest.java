5fd8f1391ce805e17828d7f981cc5cfe499c3d47
Saveli Gulas <saveli.gulas@gmail.com>
2025-04-27 15:29:17 +0200

    changed emailOrUsername to username

--- auth/src/main/java/fhv/omni/auth/controller/model/AuthenticationRequest.java
+++ auth/src/main/java/fhv/omni/auth/controller/model/AuthenticationRequest.java
@@ -3,6 +3,6 @@ package fhv.omni.auth.controller.model;
 import jakarta.validation.constraints.NotNull;
 
 public record AuthenticationRequest(
-        @NotNull String emailOrUsername,
+        @NotNull String username,
         @NotNull String password) {
 }
